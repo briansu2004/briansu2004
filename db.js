ba1732ba3cfc1c7762ca66ac54e0fa18392dfe45
briansu2004 <briansu2004@hotmail.com>
2024-01-11 17:19:21 -0500

    $

---
+++ db.js
@@ -0,0 +1,70 @@
+const coursesData = [
+  {
+    id: 1,
+    title: 'React Rapid: Rapidstart Your Responsive App',
+    url: 'https://www.udemy.com/course/react-rapidstart-creating-an-responsive-react-app-in-1-hour/',
+    logo: 'course_001.jpg',
+  },
+  {
+    id: 2,
+    title: 'Struts Savvy: Quickstart Your Java Projects',
+    url: 'https://www.udemy.com/course/struts-savvy-quickstart-your-java-projects/',
+    logo: 'course_002.jpg',
+  },
+  {
+    id: 3,
+    title: 'Spring MVC Mastery: Jumpstart Your Java Projects',
+    url: 'https://www.udemy.com/course/spring-mvc-mastery-jumpstart-your-java-projects/',
+    logo: 'course_003.jpg',
+  },
+  {
+    id: 4,
+    title: 'DevOps Labs: 16 Real DevOps Projects',
+    url: 'https://www.udemy.com/course/devops-labs-16-real-devops-projects/',
+    logo: 'course_004.jpg',
+  },
+  {
+    id: 5,
+    title: 'Efficient Kubernetes Management with Python and Go',
+    url: 'https://www.udemy.com/course/managing-dask-kubernetes-with-kubectl-python-and-go/',
+    logo: 'course_005.jpg',
+  },
+  {
+    id: 6,
+    title: 'DevOps Interview Questions',
+    url: 'https://www.udemy.com/course/devops-interview-questions-20xx/',
+    logo: 'course_006.jpg',
+  },
+  {
+    id: 7,
+    title: 'Ace Your Google Cloud (GCP) Job Interviews',
+    url: 'https://www.udemy.com/course/it-contractor-google-cloud-gcp-interview-questions-20xx/',
+    logo: 'course_007.jpg',
+  },
+  {
+    id: 8,
+    title: 'IT Behavioral Interview Questions',
+    url: 'https://www.udemy.com/course/it-contractor-behavioral-interview-questions-20xx/',
+    logo: 'course_008.jpg',
+  },
+  {
+    id: 9,
+    title: 'Maven Mastery: Kickstart Your Java Projects',
+    url: 'https://www.udemy.com/course/maven-mastery-kickstart-your-java-projects/',
+    logo: 'course_009.jpg',
+  },
+  {
+    id: 10,
+    title: 'DevOps Labs: 9 Real DevOps Projects',
+    url: 'https://www.udemy.com/course/devops-labs-9-real-devops-projects/',
+    logo: 'course_010.jpg',
+  },
+  {
+    id: 19,
+    title: 'Mendix Magic: Mobilize Your Responsive Apps with No Code',
+    url: 'https://www.udemy.com/course/mendix-magic/',
+    logo: 'course_019.jpg',
+  },
+];
+
+export default coursesData;
